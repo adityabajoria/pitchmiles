d1d8c0a79ee817166ee91f7b96d173b4b9a192c6
Aditya Bajoria <63720245+adityabajoria@users.noreply.github.com>
2025-08-29 19:09:15 -0400

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -12,9 +12,7 @@ from pathlib import Path
 
 DATASETTE_URL = "http://localhost:8001/football"
 
-
-DB_PATH = Path(__file__).resolve().parents[1] / "sql" / "football.db"
-conn = sqlite3.connect(f"file:{DB_PATH}?mode=ro", uri=True)
+conn = sqlite3.connect('/Users/aditya/datascience-projects/pitch-miles/sql/football.db')
 
 # --- GLOBAL STYLING ---
 st.markdown(
